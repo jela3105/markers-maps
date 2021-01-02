327721bd2a402609364cc0d4f967e7081691df16
jela3105 <jela3105@gmail.com>
2021-01-01 22:49:59 -0600

    Create prompt to input the point name

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { Map, Modal, Panel } from "./components";
+import { Map, Modal, Panel, Input } from "./components";
 
 export default function App() {
   const [points, setPoints] = useState([]);
+  const [name, setName] = useState("");
 
   const handleLongPress = ({ nativeEvent }) => {
     const newPoints = points.concat({ coordinate: nativeEvent.coordinate });
     setPoints(newPoints);
   };
 
-  console.log(points);
+  const handleChangeText = (text) => {};
+
   return (
     <View style={styles.container}>
       <Map onLongPress={handleLongPress} />
       <Panel />
       <Modal visibility={true}>
-        <Text>Efesota</Text>
+        <Input
+          title="Name"
+          placeholder="Point name"
+          onChangeText={handleChangeText}
+        />
       </Modal>
       <StatusBar style="auto" />
     </View>
