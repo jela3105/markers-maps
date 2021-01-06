c899c569bdee17581c7c258679220492be9c7033
jela3105 <jela3105@gmail.com>
2021-01-05 23:11:49 -0600

    Add styles to list modal

--- components/List.js
+++ components/List.js
@@ -1,17 +1,33 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import {
+  FlatList,
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 
-export default ({ points }) => {
+export default ({ points, closeModal }) => {
   console.log(points);
   return (
     <>
-      <View>
+      <View style={styles.list}>
         <FlatList
           data={points.map((x) => x.name)}
           renderItem={({ item }) => <Text>{item}</Text>}
           keyExtractor={(item) => item}
         />
       </View>
+      <View>
+        <Button onPress={closeModal} title="Cancel" />
+      </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  list: {
+    height: Dimensions.get("window").height - 300,
+  },
+});
